7a873d9508a4bb44b8dbeb2b5303d32a078943f7
Elliot <140021772+Emorgan12@users.noreply.github.com>
2024-10-09 13:17:43 +0100

    Delete Contact.Api directory

--- Contact.Api/Dtos.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-
-namespace Contact.Api.Dtos
-{
-    public record ItemDto(Guid Id, string Name, string Number, DateTimeOffset CreatedDate);
-    public record CreateItemDto([Required] string Name, [Length(11,11)] string Number);
-    public record UpdateItemDto([Required] string Name, [Length(11,11)] string Number);
-}
